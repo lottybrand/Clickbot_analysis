32cd9fd8b861bd9deb04bc1e0517b6f72cafb234
Charlotte Brand <git>
2021-06-16 10:59:56 +0100

    did we reduce No's? Yes!
    finally got my head around modelling if there was an experiment effect on vaccination intentions, that makes sense with our data (nos did decrease). Think the model_nos does this and makes sense with the data :)

--- altay_rep.R
+++ altay_rep.R
@@ -30,8 +30,48 @@ summary(altay_model)
 # this makes sense. Still want to model moves away from No to double-check. so N0 =  1 everything else 0, 
 # predict No's based on before/after and condition, so make wide -- long (and then varying intercept for ppt?)
 
-A1 <- read.csv("A1_altay.csv")
+# trying to convert intention to vaccinate (vax_future_1 and vax_future_2) into Nos or not, and predict Nos before and after? (did Nos decrease?)
+# (is it better to do it this way round than predicting Not Nos and seeing if Not Nos increase? should be symmetrical...?)
 
-table(A1$Beh_post, A1$Condition)
+colnames(clean_clickbot)
+clean_clickbot_NOs <- reshape(clean_clickbot,  
+                                    varying = list(c("vax_future_1","vax_future_2")),
+                                    v.names = c("intention"), 
+                                    direction = "long")
 
-table(clean_clickbot$vax_future_2_altay, clean_clickbot$condition)
+#change default 'time' column name to post intention (0 = pre, 1 = post)
+colnames(clean_clickbot_NOs)[colnames(clean_clickbot_NOs) == "time"] <- "post_intention"
+
+#make binary (1 = post rating, 0 = pre rating)
+clean_clickbot_NOs$post_intention <- ifelse((clean_clickbot_NOs$post_intention ==1),0,1)
+
+#make intention binary (1 = No, 0 = everything else)
+clean_clickbot_NOs$intention <- ifelse((clean_clickbot_NOs$intention ==-1),1,0)
+
+# remove that default ids column that reshape makes 
+clean_clickbot_NOs$id <- NULL
+
+#### now model this! ####
+
+#just choose the columns it wants
+no_data <- subset(clean_clickbot_NOs, select=c("ID","intention","post_intention"))
+
+model_nos <- map2stan(
+  alist(
+    intention ~ dbinom(1, p),
+    logit(p) <- a + b*post_intention,
+    a ~ dnorm(0,1),
+    b ~ dnorm(0,1)
+  ),
+  data=no_data, 
+  warmup=1000, iter=4000, chains=3, cores=3)
+
+precis(model_nos)
+
+# yes! There were less no's after the treatment, I think this is what we wanted to find out :)
+
+#     mean   sd  5.5% 94.5% n_eff Rhat
+# a  0.14 0.07  0.02  0.26  3057    1
+# b -0.37 0.11 -0.54 -0.20  3329    1
+
+
