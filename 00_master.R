b0460236d06c0bfedc7d69e44ef1a7bf81e17e85
Tom Stafford <tom@idiolect.org.uk>
2021-07-01 16:34:33 +0100

    apply tom's organisational scheme

---
+++ 00_master.R
@@ -0,0 +1,44 @@
+##########################################################
+##                       START HERE                     ##
+##
+## This script organises the project. We borrow the organisation scheme from Claudia von Bastian's R workshops
+## http://www.claudiavonbastian.com
+##
+## But notes on the general idea are here
+## https://tomstafford.github.io/psy6422/project-organisation.html
+##
+##########################################################
+
+
+##########################################################
+##                       LIBRARIES                      ##
+##########################################################
+
+#install notes and ALL libraries used go here
+library(tidyverse)
+library(here)
+
+##########################################################
+##                       SETTINGS                       ##
+##########################################################
+
+# nothing here yet
+
+
+##########################################################
+##                      PROCESSING                      ##
+##########################################################
+
+#to update when we have organised the scripts, but they'll be something like
+
+# 01: Prepare --------------------------------------------
+# load and merge the data files
+# source(paste(workspace, "1_prepare.R", sep = "/"))
+
+# 02: Analyse --------------------------------------------
+# extract and summarise the relevant variables from the data
+# source(paste(workspace, "2_process.R", sep = "/"))
+
+# 03: Supplementary file ---------------------------------
+# explore and analyse the data
+# source(paste(workspace, "3_analyse.R", sep = "/"))
--- 01_altay_rep.R
+++ 01_altay_rep.R
@@ -1,4 +1,16 @@
 
+#### Key to Altay Script ####
+
+# A0 = all data 
+# A0 Condition = "Consentform(control) and Consentform(test)" 
+
+# Bot = just the "test" (chatbot) condition dataframe 
+
+# Control = just the control condition dataframe 
+
+# D1 = Chatbot condition, made Long instead of Wide, based on pre & post attitude ratings. 
+
+# D1 = *Overwritten and created a second time* using the Control dataframe instead of Bot dataframe. 
 
 ##### trying Altay's analysis strategy... #####
 
