02968db3dd2a6e204bca7d2e79907107fe55c800
Tom Stafford <tom@idiolect.org.uk>
2021-12-16 20:04:02 +0000

    moar shiny

--- shinyapp/freetext.R
+++ shinyapp/freetext.R
@@ -6,7 +6,7 @@
 #
 #    http://shiny.rstudio.com/
 #
-
+# This was useful https://shiny.rstudio.com/articles/debugging.html
 
 library(shiny)
 
@@ -23,33 +23,40 @@ n_items <- nrow(df)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Proof of concept comment display"),
+    titlePanel("Proof of concept : free text comments from participants"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("item_n",
-                        "Comment number:",
-                        min = 1,
-                        max = n_items,
-                        step = 1,
-                        value = 5)
+            h5("Free text responses from participants, all self defining as against or unsure about vaccination. Click 'Generate' to see a random selection"),
+            actionButton("do", "Generate!") # thanks https://www.rdataguy.com/2019/11/lesson-9-random-number-generator-part-2.html
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
            #plotOutput("distPlot")
-            textOutput("text")
+            textOutput("text"),
+            tableOutput("randNumbers")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    a <- reactive(input$item_n) #get slide value
     
-    output$text <- renderText({df$anything_else[a()]})
+    # Use an action button as an event to generate the list of random numbers
+    random_data <- eventReactive(input$do, {  
+        
+        # Randomly sample values from the specified range
+        numbers <- seq(1:n_items)
+        sample(numbers, 5)
+        
+    })
+    
+    # Output the list of random numbers only AFTER the "Generate!" button is pressed
+    output$randNumbers <- renderTable({
+        df$anything_else[random_data()]
+    }, rownames = FALSE, colnames = FALSE)
     
 }
 
